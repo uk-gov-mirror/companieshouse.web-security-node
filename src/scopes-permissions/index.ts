a2a8ef96a6a63803d48454bd97ebacc90a0dffc5
bmchugh-ch <bmchugh@companieshouse.gov.uk>
2024-09-18 16:30:32 +0100

    IDVA6-1539 update exports

--- src/scopes-permissions/index.ts
+++ src/scopes-permissions/index.ts
@@ -1,3 +1,8 @@
 export {
     acspProfileCreateAuthMiddleware,
 } from './acspProfileCreateAuthMiddleware'
+
+export {
+    acspManageUsersAuthMiddleware,
+    AcspOptions
+} from './acspManageUsersAuthMiddleware'
